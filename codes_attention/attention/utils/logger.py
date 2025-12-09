93c7d4055b3c417ad6ff00ad819c3ecc94ab3bf0
JURIUENO11 <158554713+JURIUENO11@users.noreply.github.com>
2025-12-09 12:18:10 +0900

    Update logger.py

--- codes_attention/attention/utils/logger.py
+++ codes_attention/attention/utils/logger.py
@@ -5,29 +5,23 @@ from logging import FileHandler, StreamHandler
 def get_logger(name: str):
     logger = logging.getLogger(name)
 
-    # すでにハンドラーが設定されている場合はスキップ
     if logger.hasHandlers():
         return logger
 
-    # ロガーのレベル設定
     logger.setLevel(logging.INFO)
 
-    # フォーマッターの作成
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
 
-    # ファイルハンドラーの設定
     file_handler = FileHandler(f'./log/{name}.log')
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
 
-    # ストリームハンドラーの設定
     stream_handler = StreamHandler()
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(formatter)
 
-    # ハンドラーの追加
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
